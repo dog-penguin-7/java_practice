93a73b32a4a8a8b7c82438fb3891acfc1c15e9e0
pengry <dogpenguin9@gmail.com>
2018-11-23 10:42:08 +0800

    update info of class.

--- src/self/practice/string/TernaryExpressionSplite.java
+++ src/self/practice/string/TernaryExpressionSplite.java
@@ -1,4 +1,13 @@
 package self.practice.string;
 
 public class TernaryExpressionSplite {
+    public static void main() {
+        String color = "黑色";
+        String size = "L";
+
+        String com = "颜色：" + color == null ? null : color + "尺码：" + size == null ? null : size;
+
+        System.out.println(com);
+        //System.out.println("Hello World!");
+    }
 }
