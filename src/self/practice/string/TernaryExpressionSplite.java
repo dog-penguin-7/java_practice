271cbe56b4a42e074b0b3e5c960b987c69db5665
pengry <dogpenguin9@gmail.com>
2018-11-23 14:53:01 +0800

    need brackets.

--- src/self/practice/string/TernaryExpressionSplite.java
+++ src/self/practice/string/TernaryExpressionSplite.java
@@ -5,7 +5,7 @@ public class TernaryExpressionSplite {
         String color = "黑色";
         String size = "L";
 
-        String com = "颜色：" + color == null ? null : color + "尺码：" + size == null ? null : size;
+        String com = "颜色：" + (color == null ? null : color) + "尺码：" + (size == null ? null : size);
         // Will print "尺码："
         System.out.println(com);
     }
